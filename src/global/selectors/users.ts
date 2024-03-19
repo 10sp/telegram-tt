0877edd4a5f0b7b55acb6125a550348d0021947a
Shiv Patil <shivgeeky@gmail.com>
2024-03-19 11:50:45 +0530

    Premium unlocked

--- src/global/selectors/users.ts
+++ src/global/selectors/users.ts
@@ -24,7 +24,8 @@ export function selectIsUserBlocked<T extends GlobalState>(global: T, userId: st
 export function selectIsCurrentUserPremium<T extends GlobalState>(global: T) {
   if (!global.currentUserId) return false;
 
-  return Boolean(global.users.byId[global.currentUserId].isPremium);
+  // return Boolean(global.users.byId[global.currentUserId].isPremium);
+  return true;
 }
 
 export function selectIsPremiumPurchaseBlocked<T extends GlobalState>(global: T) {
